6d5239d7cb81b062d6c70a1f7ed5ccd15b212dea
Scott Balentine <sbalentine@zenefits.com>
2016-04-18 20:09:43 -0700

    dynamic timezone

--- sunrise_calculator.rb
+++ sunrise_calculator.rb
@@ -7,6 +7,7 @@ SETTINGS_FILE = File.expand_path("../settings.yml", __FILE__)
 SETTINGS = YAML.load_file(SETTINGS_FILE)
 LATITUDE = SETTINGS["coordinates"]["latitude"]
 LONGITUDE = SETTINGS["coordinates"]["longitude"]
+TIMEZONE = SETTINGS["times"]["timezone"]
 SUNRISE_API = "http://api.sunrise-sunset.org"
 SUNRISE_CLIENT = Faraday.new(SUNRISE_API)
 
@@ -26,10 +27,12 @@ sunrise_data = JSON.parse sunrise_response.body
 utc_sunrise = sunrise_data["results"]["sunrise"]
 utc_sunset = sunrise_data["results"]["sunset"]
 
-local_sunrise = Time.parse(utc_sunrise + " UTC").in_timezone("America/Los_Angeles")
-local_sunset = Time.parse(utc_sunset + " UTC").in_timezone("America/Los_Angeles")
+local_sunrise = Time.parse(utc_sunrise + " UTC").in_timezone(TIMEZONE)
+local_sunset = Time.parse(utc_sunset + " UTC").in_timezone(TIMEZONE)
 
 SETTINGS["times"]["sunrise"] = local_sunrise
 SETTINGS["times"]["sunset"] = local_sunset
 
-File.open(SETTINGS_FILE, 'w') {|f| f.write SETTINGS.to_yaml}
+file = File.open(SETTINGS_FILE, 'w')
+file.write SETTINGS.to_yaml
+file.close
