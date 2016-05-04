fdc9b84e4ef3e9bc5adb97452cf3c1d75b36579e
Scott Balentine <sbalentine@zenefits.com>
2016-05-03 21:11:56 -0700

    change hue ip address to api call

--- sunrise_hue.rb
+++ sunrise_hue.rb
@@ -2,39 +2,42 @@ require "faraday"
 require "json"
 require "time"
 require "yaml"
-require "pry"
 
 SETTINGS_FILE = File.expand_path "../settings.yml", __FILE__
 SETTINGS = YAML.load_file SETTINGS_FILE
 HUE_USERNAME = SETTINGS["hue"]["username"]
-HUE_IP = SETTINGS["hue"]["ip"]
 HUE_LIGHT_IDS = SETTINGS["hue"]["light_ids"]
-HUE_CLIENT = Faraday.new "http://#{HUE_IP}"
 
 def sunrise_soon?
-	sunrise = Time.parse SETTINGS["times"]["sunrise"]
-	(sunrise - Time.now).between? 0, 120
+  sunrise = Time.parse SETTINGS["times"]["sunrise"]
+  (sunrise - Time.now).between? 0, 120
 end
 
 def sunset_soon?
-	sunset = Time.parse SETTINGS["times"]["sunset"]
-	(sunset - Time.now).between? 0, 120
+  sunset = Time.parse SETTINGS["times"]["sunset"]
+  (sunset - Time.now).between? 0, 120
 end
 
 def change_state(state)
   HUE_LIGHT_IDS.each do |hue_id|
-    HUE_CLIENT.put "/api/#{HUE_USERNAME}/lights/#{hue_id}/state", JSON.generate(on: state)
+    hue_client.put "/api/#{HUE_USERNAME}/lights/#{hue_id}/state", JSON.generate(on: state)
   end
 end
 
+def hue_client
+  internal_api = Faraday.new "https://www.meethue.com/api/nupnp"
+  JSON.parse(internal_api.get.body)[0]["internalipaddress"]
+	Faraday.new "http://#{hue_ip}"
+end
+
 while true
-	now = Time.now
-	sunset = Time.parse SETTINGS["times"]["sunset"]
-	if sunrise_soon?
-		change_state(false)
-	end
-	if sunset_soon?
-		change_state(true)
-	end
-	sleep 60
+  now = Time.now
+  sunset = Time.parse SETTINGS["times"]["sunset"]
+  if sunrise_soon?
+    change_state false
+  end
+  if sunset_soon?
+    change_state true
+  end
+  sleep 60
 end
